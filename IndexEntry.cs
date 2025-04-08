d70f8050e4baad80040176536e8d2f60652cda7f
SofaPR21 <sofa.zykova061@gmail.com>
2025-04-08 11:40:14 +0500

    Add files via upload

---
+++ IndexEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace prKol_ind3_Zykova_v6
+{
+    class IndexEntry
+    {
+        public string Slova { get; set; }
+
+        public ArrayList Number { get; set; }
+
+        public IndexEntry(string slova, ArrayList number)
+        {
+            slova = Slova;
+            Number = new ArrayList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace prKol_ind3_Zykova_v6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - ввести с клавиатуры, 2 - вывести из файла");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 1)
+            {
+                Console.WriteLine("Введите слово указатель: ");
+                string slovo = Console.ReadLine();
+
+
+            }
+            else if (num == 2)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("неккоректный ввод. введите 1 или 2.");
+            }
+            
+        }
+    }
+}
