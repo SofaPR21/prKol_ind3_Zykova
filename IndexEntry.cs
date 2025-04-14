a0d28b0bf89e88a742795521760bcbb8573b31e1
SofaPR21 <sofa.zykova061@gmail.com>
2025-04-14 21:42:18 +0500

    Add files via upload

---
+++ IndexEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prKol_ind3_Zykova_v6
+{
+    //запись в указатель
+    internal class IndexEntry
+    {
+        public string Slova { get; set; }
+        public ArrayList PageNumbers { get; set; }
+
+        public IndexEntry(string slova)
+        {
+            Slova = slova;
+            PageNumbers = new ArrayList();
+        }
+
+        //добавление номера страниц в список
+        public void AddPageNumber(int pageNumber)
+        {
+            //пока страниц < 10 и страница не добавлена - добавляем номер
+            if (PageNumbers.Count < 10 && !PageNumbers.Contains(pageNumber))
+            {
+                PageNumbers.Add(pageNumber);
+            }
+        }
+
+        //вывод записи
+        public override string ToString()
+        {
+            return $"{Slova}: Страницы {string.Join(", ", PageNumbers.ToArray())}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using prKol_ind3_Zykova_v6;
+
+//предметный указатель
+SubjectIndex index = new SubjectIndex();
+
+Console.WriteLine("1 - ввести с клавиатуры, 2 - вывести из файла");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("Введите слово указатель: ");
+    string slovo = Console.ReadLine();
+    index.DisplayPageNumbersForWord(slovo);
+
+}
+else if (num == 2)
+{
+
+}
+else
+{
+    Console.WriteLine("неккоректный ввод. введите 1 или 2.");
+}
